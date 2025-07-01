9bce4f851b2960814d25940bedb1fff4bbc6c4a4
Maxim Hahalin <37066384+Maksus102@users.noreply.github.com>
2025-07-01 12:25:55 +0700

    serial

--- C#Scripts/ArduinoConnect.cs
+++ C#Scripts/ArduinoConnect.cs
@@ -4,6 +4,7 @@ using System.IO.Ports;
 using System.Text;
 using System.Globalization;
 using System.Collections;
+using System.Collections.Generic;
 
 public partial class ArduinoConnect : Label3D
 {
@@ -15,9 +16,10 @@ public partial class ArduinoConnect : Label3D
 	public override void _Ready()
 	{
 		sport = new SerialPort();
-		sport.PortName = "COM3";
+		sport.PortName = "COM4";
 		sport.BaudRate = 9600;
 		sport.Open();
+
 	}
 
 	public override void _Process(double delta)
@@ -25,8 +27,10 @@ public partial class ArduinoConnect : Label3D
 		try
 		{
 			if (!sport.IsOpen) return;
-			string sermess = sport.ReadExisting();
-			output = int.Parse(sermess, CultureInfo.InvariantCulture.NumberFormat);
+			byte[] mess = { 0x1 };
+            sport.Write(mess,0,1);
+            string sermess = sport.ReadLine();
+            output = int.Parse(sermess, CultureInfo.InvariantCulture.NumberFormat);
 			base.Text = sermess;
 		}
 		catch (Exception ex )
@@ -34,4 +38,32 @@ public partial class ArduinoConnect : Label3D
 			
 		}
 	}
+}
+
+public class Protocol
+{
+    public byte PacketAdress { get; private set; }
+    public byte PacketType { get; private set; }
+    public byte PacketVar1 { get; private set; }
+    public byte PacketVar2 { get; private set; }
+
+	public static Protocol Create (byte adress, byte type, byte var1, byte var2)
+	{
+		return new Protocol
+		{
+			PacketAdress = adress,
+			PacketType = type,
+			PacketVar1 = var1,
+			PacketVar2 = var2
+		};
+	}
+
+	/*public static Protocol Parse(byte[] packet)
+	{
+        if (packet.Length < 4)
+        {
+            return null;
+        }
+    }
+	*/
 }
--- arduintest/arduintest.ino
+++ arduintest/arduintest.ino
@@ -1,15 +1,18 @@
 int val = 0;
+int incomingByte = 0;
 #define POT_PIN A0
 
 void setup() {
   Serial.begin(9600);
   pinMode(POT_PIN, INPUT);
-
 }
 
 void loop() {
-
-  val = analogRead(POT_PIN);
-  Serial.println(val);
-  delay(100);
+  if (Serial.available() > 0) {  
+    incomingByte = Serial.read();
+    if (incomingByte == 1)
+    {val = analogRead(POT_PIN);
+    Serial.println(val);
+    delay(100);}
+}
 }
